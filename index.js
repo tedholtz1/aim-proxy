90eb8671b66d711e4e9561ecbc9139125937d7d6
tedholtz1 <tedholtz1@gmail.com>
2026-04-21 15:10:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,37 @@ app.use(express.json());
 
 const AIM_BASE = 'https://active-ewebservice.biz/aeServices30/api';
 
-app.all('/aim/*', async (req, res) => {
+app.all('/aim*', async (req, res) => {
   const aimPath = req.path.replace('/aim', '');
-  const url = AIM_BASE + aimPath + (req.url.includes('?') ? '?' + req.url.split('?')[1] : '');
-  
+  const queryString = req.url.includes('?') ? '?' + req.url.split('?')[1] : '';
+  const url = AIM_BASE + aimPath + queryString;
+
+  const forwardHeaders = { 'Content-Type': 'application/json' };
+  if (req.headers['apikey']) forwardHeaders['APIKey'] = req.headers['apikey'];
+  if (req.headers['appid']) forwardHeaders['AppId'] = req.headers['appid'];
+  if (req.headers['oauthtoken']) forwardHeaders['OAuthToken'] = req.headers['oauthtoken'];
+  if (req.headers['token']) forwardHeaders['Token'] = req.headers['token'];
+
+  console.log('Proxying:', req.method, url);
+  console.log('Headers:', forwardHeaders);
+
   try {
     const response = await fetch(url, {
       method: req.method,
-      headers: {
-        'Content-Type': 'application/json',
-        ...req.headers.apikey && { 'APIKey': req.headers.apikey },
-        ...req.headers.appid && { 'AppId': req.headers.appid },
-        ...req.headers.oauthtoken && { 'OAuthToken': req.headers.oauthtoken },
-        ...req.headers.token && { 'Token': req.headers.token },
-      },
+      headers: forwardHeaders,
       ...(req.method !== 'GET' && { body: JSON.stringify(req.body) })
     });
 
-    const data = await response.json();
-    res.status(response.status).json(data);
+    const text = await response.text();
+    console.log('AIM response:', text.substring(0, 200));
+
+    try {
+      res.status(response.status).json(JSON.parse(text));
+    } catch {
+      res.status(response.status).send(text);
+    }
   } catch (err) {
+    console.error('Proxy error:', err.message);
     res.status(500).json({ error: err.message });
   }
 });
